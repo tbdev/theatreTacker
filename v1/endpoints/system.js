ed54916b1585bf40ed74c7a5f825661d09c17f0b
Tyler Bowler <TBowler@ccbac.com>
2016-06-20 22:28:35 -0500

    added a POST endpoint

--- v1/endpoints/system.js
+++ v1/endpoints/system.js
@@ -10,7 +10,25 @@ module.exports = function(router, utils) {
 			res.json( Math.random() );
 		});
 
-	router.route('/v1/system/check')
+	router.route('/v1/system/check/post')
+		.post(function(req,res){
+			var token = req.app.get('token');
+			var AUTH_TOKEN = req.app.get('AUTH_TOKEN');
+			console.log( token );
+			console.log('req.body', req.body );
+
+			utils.firebase_Push( token, '/inputs/', AUTH_TOKEN).then(
+				function(good){
+					utils.sendResponse(req,res,'GOOD', true,false);
+				},function(bad){
+					utils.sendResponse(req,res,'BAD', true,false);
+				}
+			)
+
+		});
+
+
+	router.route('/v1/system/check/get')
 		.get(function(req,res){
 			var token = req.app.get('token');
 			var AUTH_TOKEN = req.app.get('AUTH_TOKEN');
